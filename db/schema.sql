90dc4dbf7ddf2a2779b6c924c3644e3ef784efb7
Nick Edwards <theadventuresofnickedwards@gmail.com>
2022-02-03 19:39:28 -0800

    schema and seeds both functional

--- db/schema.sql
+++ db/schema.sql
@@ -3,31 +3,28 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE departments (
+  id INT NOT NULL,
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE roles (
+    id INT NOT NULL,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
+    salary DECIMAL(15, 2),
     department_id INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department (id)
-    -- ON DELETE SET NULL
+    FOREIGN KEY (department_id) REFERENCES departments (id) ON DELETE SET NULL
 );
 
-CREATE TABLE employee (
-  id INT NOT NULL,
+CREATE TABLE employees (
+  id INT AUTO_INCREMENT NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
+  manager_id INT,
   PRIMARY KEY (id),
-  FOREIGN KEY (role_id) REFERENCES role (id),
-  manager_id
---   FOREIGN KEY (manager_id)
---   REFERENCES role(id)
---   ON DELETE SET NULL
+  FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) REFERENCES employees (id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,10 @@
-INSERT INTO department (id, name)
+INSERT INTO departments (id, name)
 VALUES (1, "Engineering"),
        (2, "Legal"),
        (3, "Finance"),
        (4, "Sales");
        
-INSERT INTO role (id, title, salary, department_id)
+INSERT INTO roles (id, title, salary, department_id)
 VALUES (1, "Lead Engineer", 150000, 1),
        (2, "Software Engineer", 125000, 1),
        (3, "Lawyer", 85000, 2),
@@ -13,11 +13,11 @@ VALUES (1, "Lead Engineer", 150000, 1),
        (6, "Salesperson", 80000, 4),
        (7, "Accountant", 100000, 3);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, "Mike", "Chan", "Salesperson"),
-       (2, "Ashley", "Rodriguez", "Lead Engineer"),
-       (3, "Kevin", "Tupik", "Software Engineer", 2),
-       (4, "Kunal", "Singh", "Account Manager"),
-       (5, "Malia", "Brown", "Accountant", 4),
-       (6, "Sarah", "Lourd", "Legal Team Lead"),
-       (7, "Tom", "Allen", "Lawyer", 6);
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Mike", "Chan", 6, NULL),
+       (2, "Ashley", "Rodriguez", 1, NULL),
+       (3, "Kevin", "Tupik", 2, 2),
+       (4, "Kunal", "Singh", 5, NULL),
+       (5, "Malia", "Brown", 7, 4),
+       (6, "Sarah", "Lourd", 4, NULL),
+       (7, "Tom", "Allen", 3, 6);
